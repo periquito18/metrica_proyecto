f4d53f1108ca5d54f3ebfef29bdecab53d21e68d
raulvt02 <vera.trashorras.raul@iescamas.es>
2025-05-23 14:39:18 +0200

    as

--- GymApp/src/main/java/com/instituto/gymapp/TestConexion.java
+++ GymApp/src/main/java/com/instituto/gymapp/TestConexion.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 import util.Conexion;
 
 public class TestConexion {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         try (Connection conn = Conexion.conectarBD()) {
             System.out.println("Conexión exitosa a la BD");
             Statement stmt = conn.createStatement();
